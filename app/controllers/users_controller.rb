d24b38f4b190f2a6fa1df1a6fbb7ddeff476b5fd
sandra <nyamweasandra@gmail.com>
2023-06-27 14:27:14 +0300

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
-end
+    
+        def show
+            user = User.find(params[:id])
+            render json: user, include: :items
+          end
+        # Your code to retrieve a user with an array of all items associated with that user
+      end
+
